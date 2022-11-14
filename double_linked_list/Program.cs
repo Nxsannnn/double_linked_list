95d2ca8af6a797a976d0096a1cc068bc3b4bd397
Ichsan Al Munawwar Patintingan <ichsan.al.ft21@mail.umy.ac.id>
2022-11-14 16:53:25 +0700

    Class Node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace double_linked_list
 {
+    class Node
+    {
+        public int noMhs;
+        public int name;
+        public Node next;
+        public Node prev;
+    }
+    class DoubleLinkedList
+    {
+        Node START;
+
+    }
     internal class Program
     {
         static void Main(string[] args)
